a7187f886206c9f7fd0cfa71ea13267b2ebd35cc
longjinwen <dxfjyygy@sina.com>
2017-11-27 09:13:02 +0800

    add test

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,8 +1,18 @@
+import java.io.*;
+import java.util.Properties;
+
 /**
  * Created by longjinwen on 23/11/2017.
  */
 public class HelloGit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("hello Git!!");
+        Properties properties = new Properties();
+       // InputStream in = new FileInputStream(new File("test.properties"));
+        InputStream in = HelloGit.class.getClassLoader().getResourceAsStream("test.properties");
+        properties.load(in);
+        // add test char
+        System.setProperties(properties);
+        System.out.println(System.getProperty("asdf"));
     }
 }
